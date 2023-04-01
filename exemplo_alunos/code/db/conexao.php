ca5b0fa614702b727df39dbdff802e719ff8853b
Lucas Faria <lucasljf@gmail.com>
2023-03-31 21:58:18 -0300

    add class conexao

---
+++ exemplo_alunos/code/db/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+class Conexao
+{
+    private $host = 'mariadb-server';
+    private $db = 'db_exemplo_alunos;';
+    private $user = 'root';
+    private $password = '123';
+
+    public function conectar()
+    {
+        try {
+            $conexao = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->password);
+            echo "sucesso!";
+            return $conexao;
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+}
