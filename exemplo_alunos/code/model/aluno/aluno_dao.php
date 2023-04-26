2f4ac95d56e4d3ba11d1c3e42e43d8235fa1801e
Mayko Diouzef <maykodiouzef+github@gmail.com>
2023-04-26 03:48:19 -0300

    inserir e listar curso

---
+++ exemplo_alunos/code/model/aluno/aluno_dao.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once '../../db/conexao.php';
+require_once 'aluno.php';
+
+class AlunoDao
+{
+  private $conexao;
+
+  public function __construct(Conexao $conexao)
+  {
+    $this->conexao = $conexao->conectar();
+  }
+
+  public function inserir(Aluno $aluno)
+  {
+    // monta SQL
+    $sql = 'INSERT INTO tb_aluno (nome, endereco, telefone, data_nascimento) VALUES (:nome, :endereco, :telefone, :data_nascimento)';
+
+    // preencher SQL com dados do aluno que eu quero inserir
+    $stmt = $this->conexao->prepare($sql);
+    $stmt->bindValue(':nome', $aluno->__get('nome'));
+    $stmt->bindValue(':endereco', $aluno->__get('endereco'));
+    $stmt->bindValue(':telefone', $aluno->__get('telefone'));
+    $stmt->bindValue(':data_nascimento', $aluno->__get('data_nascimento'));
+
+    // manda executar SQL
+    $stmt->execute();
+  }
+
+  public function listar_tudo()
+  {
+    $sql = 'SELECT * FROM tb_aluno';
+    $stmt = $this->conexao->prepare($sql);
+    $stmt->execute();
+
+    $resultados = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $alunos = array();
+
+    // percorrer resultados
+    foreach ($resultados as $item) {
+
+      // instanciar aluno novo
+      $novo_aluno = new Aluno($item->id, $item->nome, $item->endereco, $item->telefone, $item->data_nascimento);
+
+      // guardar num novo array
+      $alunos[] = $novo_aluno;
+    }
+    // retornar esse novo array
+    return $alunos;
+  }
+}
---
+++ exemplo_alunos/code/public/aluno/inserir_aluno.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../../model/aluno/aluno.php';
+require_once '../../model/aluno/aluno_dao.php';
+
+$nome = $_GET['nome'];
+$endereco = $_GET['endereco'];
+$telefone =  $_GET['telefone'];
+$data_nascimento = $_GET['data_nascimento'];
+
+// echo $nome;
+
+$aluno = new Aluno(0, $nome, $endereco, $telefone, $data_nascimento);
+
+$conexao = new Conexao();
+$alunoDao = new AlunoDao($conexao);
+
+$alunoDao->inserir($aluno);
+
+// echo '<pre>';
+// print_r($aluno);
+// echo '</pre>';
+
+header('Location: ../index.html');
+
