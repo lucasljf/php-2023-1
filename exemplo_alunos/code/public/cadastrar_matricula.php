622cdebba673dd2cf2871c1caa78af0651a3da75
luiz-alencar <luiz.alencar@estudante.ifgoiano.edu.br>
2023-04-27 22:21:44 -0300

    ajuste em selec

---
+++ exemplo_alunos/code/public/cadastrar_matricula.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <form action="inserir_matricula.php" method="get">
+
+        Aluno: <br>
+        <select type="text" name="id_aluno" id="id_aluno"> <br><br>
+        <?php
+                require_once "../model/aluno_dao.php";
+                $conexao = new Conexao();
+                $alunoDao = new AlunoDao($conexao);
+
+                $alunos = $alunoDao -> listar_tudo();
+
+                foreach ($alunos as $aluno) {
+                    echo "<option value ='" . $aluno->id . "'>" . $aluno->nome . "</option";
+                }
+            ?>
+        </select> <br>
+
+        Turma: <br>
+        <select type="text" name="id_turma" id="id_turma"> <br><br>
+            <?php
+                require_once '../model/turma_dao.php';
+                $conexao = new Conexao();
+                $turmaDao = new TurmaDao($conexao);
+
+                $turmas = $turmaDao -> listar_tudo();
+
+                foreach ($turmas as $turma) {
+                    echo "<option value ='" . $turma->id . "'>" . $turma->nome . "</option>";
+                }
+            ?>
+        </select> <br> <br>
+
+        Data de ingresso: <br>
+        <input type="date" name="data_ingresso" id="data_ingresso"> <br><br>
+
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
---
+++ exemplo_alunos/code/public/cadastrar_turma.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <form action="inserir_turma.php" method="get">
+
+        Nome da turma: <br>
+        <input type="text" name="nome" id="nome"> <br><br>
+
+            Escolha um curso: <br>
+            <select name="curso_id" id="curso_id">
+                <?php
+                require_once '../model/curso_dao.php';
+                $conexao = new Conexao();
+                $cursoDao = new CursoDao($conexao);
+
+                $cursos = $cursoDao -> listar_tudo();
+
+                foreach ($cursos as $curso) {
+                    echo "<option value='" . $curso->id . "'>" . $curso -> nome . "</option>";
+                }
+                ?>
+            </select> <br> <br>
+         <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
