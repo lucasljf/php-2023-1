6c460c0f8f3c529f6738ebe22c1c995d5a3b99ea
Lucas Faria <lucasljf@gmail.com>
2023-05-02 14:01:29 -0300

    Add turma, turma dao e teste

---
+++ exemplo_alunos/code/test/teste_turma_dao.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../model/turma_dao.php';
+require_once '../model/curso_dao.php';
+require_once '../model/curso.php';
+
+$conexao = new Conexao();
+
+$turmaDao = new TurmaDao($conexao);
+$cursoDao = new CursoDao($conexao);
+
+$curso = $cursoDao->buscar_id(2);
+$turma = new Turma(0, "Turma de 2010", $curso);
+
+// $turmaDao->inserir($turma);
+
+$turmas = $turmaDao->listar_tudo();
+
+echo "<pre>";
+print_r($turmas);
+echo "</pre>";
