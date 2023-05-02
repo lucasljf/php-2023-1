acb2ee180c90ad16e007e4eb5e4798848f9cda39
Lucas Faria <lucasljf@gmail.com>
2023-05-02 16:19:21 -0300

    Populando o banco com exemplos

--- exemplo_alunos/code/db/banco.sql
+++ exemplo_alunos/code/db/banco.sql
@@ -12,6 +12,10 @@ CREATE TABLE `tb_aluno` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO `tb_aluno` (`id`, `nome`, `endereco`, `telefone`, `data_nascimento`) VALUES
+(1,	'Fulano',	'Rua F1',	'62987654321',	'2010-10-01'),
+(2,	'Ciclano',	'Rua C1',	'62999222222',	'1990-12-30'),
+(3,	'Teste3',	'Rua 3',	'62991003333',	'1993-03-03');
 
 DROP TABLE IF EXISTS `tb_curso`;
 CREATE TABLE `tb_curso` (
@@ -24,6 +28,9 @@ CREATE TABLE `tb_curso` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO `tb_curso` (`id`, `nome`, `descricao`, `carga_horaria`, `data_inicio`, `data_fim`) VALUES
+(1,	'Tec. Teste',	'Curso para teste do CursoDao',	200,	'2000-01-30',	'2000-03-30'),
+(2,	'Tec. Info',	'Técnico em Informática (Teste 1)',	1000,	'2011-01-01',	'2012-01-01');
 
 DROP TABLE IF EXISTS `tb_matricula`;
 CREATE TABLE `tb_matricula` (
@@ -38,6 +45,9 @@ CREATE TABLE `tb_matricula` (
   CONSTRAINT `tb_matricula_ibfk_2` FOREIGN KEY (`id_turma`) REFERENCES `tb_turma` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO `tb_matricula` (`id`, `id_aluno`, `id_turma`, `data_matricula`) VALUES
+(1,	3,	2,	'2020-01-01'),
+(2,	2,	1,	'2023-05-01');
 
 DROP TABLE IF EXISTS `tb_turma`;
 CREATE TABLE `tb_turma` (
@@ -48,3 +58,8 @@ CREATE TABLE `tb_turma` (
   KEY `id_curso` (`id_curso`),
   CONSTRAINT `tb_turma_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `tb_curso` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO `tb_turma` (`id`, `nome`, `id_curso`) VALUES
+(1,	'Turma 2009',	1),
+(2,	'Turma de 2010',	2),
+(3,	'2a',	2);
